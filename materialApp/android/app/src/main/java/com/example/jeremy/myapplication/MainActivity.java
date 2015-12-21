6c5722f385e5c5d253828cf603a7389bf06c4a7c
appcoreopc <kepung@gmail.com>
2015-12-21 17:15:49 +0800

    Added cardview + DownloadImageTask

--- materialApp/android/app/src/main/java/com/example/jeremy/myapplication/MainActivity.java
+++ materialApp/android/app/src/main/java/com/example/jeremy/myapplication/MainActivity.java
@@ -2,16 +2,14 @@ package com.example.jeremy.myapplication;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.jeremy.myapplication.FriendModule.FriendFragment;
 
 public class MainActivity extends AppCompatActivity implements FragmentDrawer.FragmentDrawerListener {
 
--- materialApp/android/app/src/main/java/com/example/jeremy/myapplication/MessageFragment.java
+++ materialApp/android/app/src/main/java/com/example/jeremy/myapplication/MessageFragment.java
@@ -3,14 +3,26 @@ package com.example.jeremy.myapplication;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.jeremy.myapplication.FriendModule.Friend;
+import com.example.jeremy.myapplication.FriendModule.FriendsAdapter;
+import com.example.jeremy.myapplication.MessageModule.MessageAdapter;
+import com.example.jeremy.myapplication.MessageModule.MessageInfo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Jeremy on 20/12/2015.
  */
 public class MessageFragment extends Fragment {
+
+    private RecyclerView recyclerView;
 
     public MessageFragment() {
         // Required empty public constructor
@@ -24,9 +36,14 @@ public class MessageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        View layout = inflater.inflate(R.layout.fragment_message, container, false);
         // Inflate the layout for this fragment
-        return rootView;
+
+        recyclerView = (RecyclerView) layout.findViewById(R.id.messagelist);
+        MessageAdapter adapter = new MessageAdapter(getActivity(), getFakeFriendInfo());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return layout;
     }
 
     @Override
@@ -38,4 +55,15 @@ public class MessageFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
     }
+
+
+    private List<MessageInfo> getFakeFriendInfo()
+    {
+        List<MessageInfo> list = new ArrayList<MessageInfo>();
+        MessageInfo f1 = new MessageInfo("Sunset", "A beautiful castle around the lake transpire tranquility", "http://www.goodlightscraps.com/content/nature/nature-images-67.jpg");
+        MessageInfo f2 = new MessageInfo("Waterfall", "The beautiful sunset in the grand canyon.", "http://www.goodlightscraps.com/content/nature/nature-images-64.jpg");
+        list.add(f1); list.add(f2);
+        return list;
+    }
+
 }
